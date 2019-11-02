56ccf691dfa7b01a835bf6a3733ec4f40648576f
leonardomorita <leonardomorita1@edu.unifil.br>
2019-11-02 03:08:47 -0300

    commit

--- codigos/src/Lagrange.java
+++ codigos/src/Lagrange.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.regexp.internal.RE;
-
 public class Lagrange {
     private double numero0;
     private double numero1;
@@ -16,6 +14,14 @@ public class Lagrange {
         final double L1 = ((x - numero0) / (numero1 - numero0)) * ((x - numero2) / (numero1 - numero2));
         final double L2 = ((x - numero0) / (numero2 - numero0)) * ((x - numero1) / (numero2 - numero1));
 
+        /*
+        L0:
+        0-1-2 -> 1,2
+
+        L1:
+        0-1-2 -> 0,2
+         */
+
         final double RESULTADO = ((LO * 1.221) + (L1 * 3.32) + (L2 * 4.953));
 
         System.out.println("Valor do L0: " + LO);
@@ -24,9 +30,68 @@ public class Lagrange {
         System.out.printf("Resultado do P(%f): %f", x, RESULTADO);
     }
 
-    public void interpolarPontos(double x, double numero0, double numero1) {
-        final double LO = ((x - numero1) / (numero0 - numero1)) * ((x - numero0) / (numero1 - numero0));
+    public void interpolarPontos2(double x, double numero0, double numero1, double numero2) {
+        double[] a = {numero0, numero1, numero2};
 
-        System.out.println("Valor do x: " + LO);
+        double[] doubles = retornarResultadoL(x, a);
+        double resultado = 0.0;
+        for (int i = 0; i <= doubles.length; i++) {
+            if (i == 0) {
+                resultado += (doubles[i] * 1.221);
+            } else if (i == 1) {
+                resultado += (doubles[i] * 3.32);
+            } else if (i == 2) {
+                resultado += (doubles[i] * 4.953);
+            }
+        }
+        System.out.println("Resultado: " + resultado);
+    }
+
+    public void interpolarPontos3(double x, double... numeros) {
+        double[] valoresDoX = new double[numeros.length];
+        valoresDoX = adicionarElementosNoArray(valoresDoX, valoresDoX);
+
+        double[] doubles = retornarResultadoL(x, valoresDoX);
+        double resultado = 0.0;
+        for (int i = 0; i <= doubles.length; i++) {
+            if (i == 0) {
+                resultado += (doubles[i] * 1.221);
+            } else if (i == 1) {
+                resultado += (doubles[i] * 3.32);
+            } else if (i == 2) {
+                resultado += (doubles[i] * 4.953);
+            }
+        }
+        System.out.println("Resultado: " + resultado);
+    }
+
+    private double[] calcularFuncaoDoValorX(double... valores) {
+        double e = Math.E;
+        double[] resultadoDaFuncao = new double[valores.length];
+        for (int i = 0; i < valores.length; i++) {
+            resultadoDaFuncao[i] = Math.pow(e, (2 * valores[i]));
+        }
+        return resultadoDaFuncao;
+    }
+
+    private double[] adicionarElementosNoArray(double[] arrayACarregar, double[] arrayCarregado) {
+        for (int i = 0; i < arrayCarregado.length; i++) {
+            arrayACarregar[i] = arrayCarregado[i];
+        }
+        return arrayACarregar;
+    }
+
+    private double[] retornarResultadoL(double x, double... numeros) {
+        double[] array = new double[numeros.length];
+        for (int i = 0; i < numeros.length; i++) {
+            double L = 1.0;
+            for (int j = 0; j < numeros.length; j++) {
+                if (i != j) {
+                    L *= ((x - numeros[j]) / (numeros[i] - numeros[j]));
+                }
+            }
+            array[i] = L;
+        }
+        return array;
     }
 }
--- codigos/src/Main.java
+++ codigos/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         Scanner teclado = new Scanner(System.in);
 
         System.out.println("Inserir os valores:");
+        System.out.print("Inserir a quantidade de pontos: ");
+        int quantidadeDePontos = teclado.nextInt();
         System.out.print("Número 1: ");
         double numero1 = teclado.nextDouble();
         System.out.print("Número 2: ");
@@ -16,7 +18,7 @@ public class Main {
         double x = teclado.nextDouble(); teclado.nextLine();
 
         Lagrange lagrange = new Lagrange(numero1, numero2, numero3);
-        lagrange.interpolarPontos(x, numero1, numero2, numero3);
+        lagrange.interpolarPontos2(x,numero1, numero2, numero3);
     }
 
 }
