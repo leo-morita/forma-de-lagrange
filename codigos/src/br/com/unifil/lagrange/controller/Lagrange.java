1f7d36a8e656680a2f7ae6144e52c3ecbe15156b
Leonardo Morita <leonardomorita2@gmail.com>
2019-11-03 17:09:24 -0300

    commit

--- codigos/src/br/com/unifil/lagrange/controller/Lagrange.java
+++ codigos/src/br/com/unifil/lagrange/controller/Lagrange.java
@@ -17,7 +17,7 @@ public class Lagrange {
         for (int i = 0; i < valoresDeL.length; i++) {
             resultado += (valoresDeL[i] * valoresDaFuncao[i]);
         }
-        System.out.println("Resultado: " + getResultado());
+        //System.out.println("Resultado: " + getResultado());
     }
 
 
@@ -52,19 +52,6 @@ public class Lagrange {
             System.out.printf("%12f | ", lista[i]);
         }
         System.out.println();
-    }
-
-    public void interpolarPontos2(double x, double numero0, double numero1, double numero2) {
-        final double LO = ((x - numero1) / (numero0 - numero1)) * ((x - numero2) / (numero0 - numero2));
-        final double L1 = ((x - numero0) / (numero1 - numero0)) * ((x - numero2) / (numero1 - numero2));
-        final double L2 = ((x - numero0) / (numero2 - numero0)) * ((x - numero1) / (numero2 - numero1));
-
-        final double RESULTADO = ((LO * 1.221) + (L1 * 3.32) + (L2 * 4.953));
-
-        System.out.println("Valor do L0: " + LO);
-        System.out.println("Valor do L1: " + L1);
-        System.out.println("Valor do L2: " + L2);
-        System.out.printf("Resultado do P(%f): %f", x, RESULTADO);
     }
 
     private double[] calcularFuncaoDoValorX(double... valores) {
--- codigos/src/br/com/unifil/lagrange/view/Tela.java
+++ codigos/src/br/com/unifil/lagrange/view/Tela.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package método.de.lagrange;
+package br.com.unifil.lagrange.view;
 
-/**
- *
- * @author victo
- */
+import br.com.unifil.lagrange.controller.Lagrange;
+
+import java.util.Locale;
+
 public class Tela extends javax.swing.JFrame {
 
     /**
@@ -16,6 +16,7 @@ public class Tela extends javax.swing.JFrame {
      */
     public Tela() {
         initComponents();
+        this.setLocationRelativeTo(null);
     }
 
     /**
@@ -38,6 +39,7 @@ public class Tela extends javax.swing.JFrame {
         jLabel5 = new javax.swing.JLabel();
         Resultado = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -48,13 +50,13 @@ public class Tela extends javax.swing.JFrame {
         jLabel2.setText(")");
 
         Tabela.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null},
-                {null, null}
-            },
-            new String [] {
-                "0", "1", "2", "3", "4"
-            }
+                new Object [][] {
+                        {null, null},
+                        {null, null}
+                },
+                new String [] {
+                        "0", "1", "2", "3", "4"
+                }
         ));
         jScrollPane2.setViewportView(Tabela);
 
@@ -77,98 +79,150 @@ public class Tela extends javax.swing.JFrame {
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jLabel6.setText("Forma de Lagrange");
 
+        jButton2.setText("Limpar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap(40, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addGroup(layout.createSequentialGroup()
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(P, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jLabel5)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(26, 26, 26)
+                                                .addComponent(jLabel1)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addGap(0, 0, Short.MAX_VALUE)
+                                                                .addComponent(jLabel3)
+                                                                .addGap(0, 0, Short.MAX_VALUE))
+                                                        .addComponent(P, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addComponent(jLabel4)))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createSequentialGroup()
-                                        .addGap(0, 0, Short.MAX_VALUE)
-                                        .addComponent(jButton1))
-                                    .addComponent(Resultado))))
-                        .addGap(72, 72, 72))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel6)
-                        .addGap(118, 118, 118))))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(jLabel2)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(jLabel5)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(Resultado))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                                        .addGroup(layout.createSequentialGroup()
+                                                                .addComponent(jButton2)
+                                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                                .addComponent(jButton1)))
+                                                .addGap(0, 0, Short.MAX_VALUE)))
+                                .addGap(72, 72, 72))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(144, Short.MAX_VALUE)
+                                .addComponent(jLabel6)
+                                .addGap(118, 118, 118))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(21, 21, 21)
-                        .addComponent(jLabel6)
-                        .addGap(40, 40, 40)
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jLabel3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jLabel4)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addGap(6, 6, 6)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel2)
-                    .addComponent(P, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(Resultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(27, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(21, 21, 21)
+                                                .addComponent(jLabel6)
+                                                .addGap(40, 40, 40)
+                                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 64, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                                .addContainerGap()
+                                                .addComponent(jLabel3)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(jLabel4)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jButton1)
+                                        .addComponent(jButton2))
+                                .addGap(6, 6, 6)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                        .addComponent(jLabel5)
+                                        .addComponent(jLabel2)
+                                        .addComponent(P, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(Resultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addContainerGap(27, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+    private Lagrange l = new Lagrange();
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        if(vNull() == false && vP() == false){//essa condição só funciona quando todos elementos estao preenchidos           
-            Resultado.setText("estao preenchido");
+
+        if(vP() == false && vNull() == false){//essa condição só funciona quando todos elementos estao preenchidos
+            l.interpolarPontos(Double.parseDouble(P.getText()), valorX());
+            String resultadoP = String.format("%.4f", l.getResultado());
+            Resultado.setText(String.valueOf(resultadoP));
+            double aux[] = l.getValoresDaFuncao();
+            for(int i = 0;i < aux.length;i++){
+                String resultado = String.format("%.4f", aux[i]);
+                Tabela.setValueAt(resultado,1,i);
+            }
         }else{
             Resultado.setText("não preenchido");
         }
-        System.out.println(x);
+
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+        for (int i = 0; i < 2; i++) {
+            for(int j = 0; j < 5; j++) {
+                Tabela.setValueAt(null, i, j);
+            }
+        }
+        P.setText(null);
+        Resultado.setText(null);
+        l = new Lagrange();
+        x = 0;
+    }//GEN-LAST:event_jButton2ActionPerformed
+
     int x = 0;
     public boolean vNull(){
         boolean v = true;
         for(int i = 0;i < 5; i++){
             if(Tabela.getValueAt(0, i) != null){
-              v = false;
-              x++; 
+                v = false;
+                x++;
             }
         }
         return v;
     }
-    
+    Object[] dados;
+    public Double[] valorX(){
+        Double[] aux = new Double[x];
+        dados = new Object[x];
+        for(int i = 0;i < 5; i++){
+            if(Tabela.getValueAt(0, i) != null){
+                dados[i] = Tabela.getValueAt(0,i);
+            }
+        }
+        for(int i = 0;i < x; i++){
+            aux[i] = Double.parseDouble(dados[i].toString());
+        }
+        return aux;
+    }
+
     public boolean vP(){
-        boolean v = true;
-        if(P.getText() != null){
-            v = false;
+        boolean v = false;
+        if(P.getText().equals("")){
+            v = true;
         }
         return v;
     }
-    
+
     /**
      * @param args the command line arguments
      */
@@ -176,7 +230,7 @@ public class Tela extends javax.swing.JFrame {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
@@ -209,6 +263,7 @@ public class Tela extends javax.swing.JFrame {
     private javax.swing.JTextField Resultado;
     private javax.swing.JTable Tabela;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
