ea0b03f1922110743300f985886b708478a161d8
leonardomorita <leonardomorita1@edu.unifil.br>
2019-11-21 01:44:45 -0300

    commit

--- codigos/src/br/com/unifil/lagrange/controller/FormaDeNewton.java
+++ codigos/src/br/com/unifil/lagrange/controller/FormaDeNewton.java
@@ -2,42 +2,123 @@ package br.com.unifil.lagrange.controller;
 
 public class FormaDeNewton {
     public void interpolar(double valorDeX, Double[] numeros, Double[] numerosDaFuncao) {
-        Double resultado;
-        Double[] resultadosOrdem = new Double[numerosDaFuncao.length-1];
-        Double[] resutadosDeD = retornarResultadoD(valorDeX, numeros);
 
-        int contador = numerosDaFuncao.length-1;
-        while (contador >= 0) {
-            resultadosOrdem = new Double[numerosDaFuncao.length];
-            for (int i = 0; i < contador; i++) {
+        Double resultado;
+        Double[] resultadosOrdem;
+//        Double[] resutadosDeD = retornarResultadoD(valorDeX, numeros);
+
+        int contador = numerosDaFuncao.length;
+        resultadosOrdem = new Double[numerosDaFuncao.length];
+
+        if (contador == 2) { // Se tiver 2 valores de x
+            calcularOrdem1(numeros, numerosDaFuncao, resultadosOrdem, contador);
+            final double P =
+                    numerosDaFuncao[0] + (valorDeX - numeros[0]) * resultadosOrdem[0] + (valorDeX - numeros[0]) *
+                            (valorDeX - numeros[1]);
+            imprimirP(valorDeX, P);
+
+        } else if (contador == 3) { // Se tiver 3 valores de x
+            for (int i = 0; i < contador-1; i++) {
                 resultado = (numerosDaFuncao[i + 1] - numerosDaFuncao[i]) / (numeros[i + 1] - numeros[i]);
                 resultadosOrdem[i] = resultado;
             }
-            contador--;
-        }
 
+            Double[] resultadosOrdem2 = new Double[contador-2];
+            calcularOrdem2(numeros, resultadosOrdem, contador, resultadosOrdem2);
 
+            final double P =
+                    numerosDaFuncao[0] + (valorDeX - numeros[0]) * resultadosOrdem[0] + (valorDeX - numeros[0]) *
+                            (valorDeX - numeros[1]) * resultadosOrdem2[0];
+            imprimirP(valorDeX, P);
+        } else if (contador == 4) { // Se tiver 4 valores de x
+            calcularOrdem1(numeros, numerosDaFuncao, resultadosOrdem, contador);
 
-        for (int i = 0; i < resultadosOrdem.length; i++) {
-            System.out.println(resultadosOrdem[i]);
+            Double[] resultadosOrdem2 = new Double[contador-2];
+            calcularOrdem2(numeros, resultadosOrdem, contador, resultadosOrdem2);
+
+            Double[] resultadosOrdem3 = new Double[contador-3];
+            calcularOrdem3(numeros, contador, resultadosOrdem2, resultadosOrdem3);
+
+            final double P =
+                    numerosDaFuncao[0] + (valorDeX - numeros[0]) * resultadosOrdem[0] + (valorDeX - numeros[0]) *
+                            (valorDeX - numeros[1]) * resultadosOrdem2[0] + (valorDeX - numeros[0]) *
+                            (valorDeX - numeros[1]) * (valorDeX - numeros[2]) * resultadosOrdem3[0];
+            imprimirP(valorDeX, P);
+        } else if (contador == 5) { // Se tiver 5 valores de x
+            calcularOrdem1(numeros, numerosDaFuncao, resultadosOrdem, contador);
+
+            Double[] resultadosOrdem2 = new Double[contador-2];
+            calcularOrdem2(numeros, resultadosOrdem, contador, resultadosOrdem2);
+
+            Double[] resultadosOrdem3 = new Double[contador-3];
+            calcularOrdem3(numeros, contador, resultadosOrdem2, resultadosOrdem3);
+
+            Double[] resultadosOrdem4 = new Double[contador-4];
+            calcularOrdem4(numeros, contador, resultadosOrdem3, resultadosOrdem4);
+
+            final double P =
+                    numerosDaFuncao[0] + (valorDeX - numeros[0]) * resultadosOrdem[0] + (valorDeX - numeros[0]) *
+                            (valorDeX - numeros[1]) * resultadosOrdem2[0] + (valorDeX - numeros[0]) *
+                            (valorDeX - numeros[1]) * (valorDeX - numeros[2]) * resultadosOrdem3[0] +
+                            (valorDeX - numeros[0]) * (valorDeX - numeros[1]) * (valorDeX - numeros[2]) *
+                                    (valorDeX - numeros[3]) * resultadosOrdem4[0];
+            imprimirP(valorDeX, P);
         }
     }
 
-    public Double[] retornarResultadoD(double valorDeX, Double... numeros) {
+    private void calcularOrdem1(Double[] numeros, Double[] numerosDaFuncao, Double[] resultadosOrdem, int contador) {
+        Double resultado;
+        for (int i = 0; i < contador-1; i++) {
+            resultado = (numerosDaFuncao[i + 1] - numerosDaFuncao[i]) / (numeros[i + 1] - numeros[i]);
+            resultadosOrdem[i] = resultado;
+        }
+    }
+
+    private void calcularOrdem2(Double[] numeros, Double[] resultadosOrdem, int contador, Double[] resultadosOrdem2) {
+        Double resultado;
+        for (int i = 0; i < contador-2; i++) {
+            resultado = (resultadosOrdem[i + 1] - resultadosOrdem[i]) / (numeros[i + 2] - numeros[i]);
+            resultadosOrdem2[i] = resultado;
+        }
+    }
+
+    private void calcularOrdem3(Double[] numeros, int contador, Double[] resultadosOrdem2, Double[] resultadosOrdem3) {
+        Double resultado;
+        for (int i = 0; i < contador-3; i++) {
+            resultado = (resultadosOrdem2[i + 1] - resultadosOrdem2[i] / (numeros[i + 3] - numeros[i]));
+            resultadosOrdem3[i] = resultado;
+        }
+    }
+    private void calcularOrdem4(Double[] numeros, int contador, Double[] resultadosOrdem3, Double[] resultadosOrdem4) {
+        Double resultado;
+        for (int i = 0; i < contador-4; i++) {
+            resultado = (resultadosOrdem3[i + 1] - resultadosOrdem3[i] / numeros[i + 4] - numeros[i]);
+            resultadosOrdem4[i] = resultado;
+        }
+    }
+
+    private void imprimirP(double valorDeX, double p) {
+        System.out.printf("P(%.4f) = %.4f", valorDeX, p);
+    }
+
+    /*public Double[] retornarResultadoD(double valorDeX, Double... numeros) {
         Double[] arrayRetorno = new Double[numeros.length];
 
         for (int i = 0; i < numeros.length; i++) {
             Double D = 1.0;
+            if (i == 0) {
+                arrayRetorno[i] = 1.0;
+            }
             for (int j = 0; j < i; j++) {
                 D *= (valorDeX + numeros[j]);
             }
-            if (i == 0) {
-                arrayRetorno[i] = numeros[i];
-            } else {
+
+            if (i != 0) {
                 arrayRetorno[i] = D;
             }
+
         }
 
         return arrayRetorno;
-    }
+    }*/
 }
--- codigos/src/br/com/unifil/lagrange/view/Main2.java
+++ codigos/src/br/com/unifil/lagrange/view/Main2.java
@@ -7,9 +7,9 @@ public class Main2 {
         FormaDeNewton formaDeNewton = new FormaDeNewton();
 
 
-        double x = 1.5;
-        Double[] numeros = {-2.0, -1.0, 0.0, 1.0, 2.0}; // 3.5 5
-        Double[] numerosDaFuncao = {-2.0, 29.0, 30.0, 31.0, 62.0};
+        double x = 1.17;
+        Double[] numeros = {1.0, 1.1, 1.3, 1.4};
+        Double[] numerosDaFuncao = {-1.0, 1.51, 2.56, -3.1};
         formaDeNewton.interpolar(x, numeros, numerosDaFuncao);
 
 //        Double[] b = formaDeNewton.retornarResultadoD(x, numeros);
